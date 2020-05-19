21b14d14c3d9095683d02c0fae66e3848200bc09
LAPTOP-AADIG9CF\简&单 <wuchen@ikuijia.com>
2020-05-19 15:00:19 +0800

    prop-types与defaultProps

--- React16.4 快速上手/src/TodoItem.js
+++ React16.4 快速上手/src/TodoItem.js
@@ -2,11 +2,14 @@
  * @Author: wuchen
  * @Date: 2019-12-13 11:44:09
  * @LastEditors: wuchen
- * @LastEditTime: 2020-04-28 19:43:56
+ * @LastEditTime: 2020-05-19 14:56:41
  * @Description: 
+ * prop-types:传值的字段类型
+ * defaultProps:默认值(若父组件没有给子组件传值,可在子组件写个默认值,防止报错)
  * @Email: rangowu@163.com
  */
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';//定义类型
 
 class TodoItem extends Component {
 
@@ -22,14 +25,26 @@ class TodoItem extends Component {
 
     render() {
         // es6解构赋值
-        const { content } = this.props;//es6
+        const { content, test } = this.props;//es6
         return (
             // <div onClick={this.handleDelete}>{this.props.content}</div>
-            <div onClick={this.handleDelete}>{content}</div>
+            <div onClick={this.handleDelete}>{test} - {content}</div>
             // 可以输入html标签并编译
             // <div onClick={this.handleDelete} dangerouslySetInnerHTML={{__html:content}}></div>
         )
     }
 }
 
+// 限制要求父组件给子组件传值的类型
+TodoItem.propTypes = {
+    content: PropTypes.string,//字符串,若是函数则为PropTypes.func
+    test: PropTypes.string.isRequired,//此字段必须从父组件传值,且为必传,若父组件未传,可定义个默认值
+    // 若可以是好几种类型,可用arrayOf
+    // content: PropTypes.arrayOf(PropTypes.number, PropTypes.string)
+}
+
+// 定义默认值
+TodoItem.defaultProps = {
+    test:'hellow world'
+}
 export default TodoItem;
